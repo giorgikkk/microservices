f34af987a9f15101398cfa2b2b86f35e0ac68ae4
giorgikkk <kgiorrgi@gmail.com>
2023-07-24 23:41:23 +0400

    service-agency-service done

--- service-agency-web/src/main/java/com/example/Main.java
+++ service-agency-web/src/main/java/com/example/Main.java
@@ -7,15 +7,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "dev.omedia.repository")
-@EntityScan(basePackages = "dev.omedia.domain")
+@EnableJpaRepositories(basePackages = "com.example.repository")
+@EntityScan(basePackages = "com.example.domain")
 @ComponentScan(basePackages = {
-        "dev.omedia.repository",
-        "dev.omedia.service",
-        "dev.omedia.controller",
-        "dev.omedia.controllerExceptionHandler",
-        "dev.omedia.config",
-        "dev.omedia.receiverService"
+        "com.example.repository",
+        "com.example.service",
+        "com.example.controller",
+        "com.example.controllerExceptionHandler",
+        "com.example.config",
+        "com.example.receiverService"
 })
 public class Main {
     public static void main(String[] args) {
